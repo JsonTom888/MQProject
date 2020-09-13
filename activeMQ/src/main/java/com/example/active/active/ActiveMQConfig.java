a4137129c86081a753b4937f7b21581da34caebb
JsonTom888 <Good666888@163.com>
2020-09-13 17:43:29 +0800

    修改注释

--- activeMQ/src/main/java/com/example/active/active/ActiveMQConfig.java
+++ activeMQ/src/main/java/com/example/active/active/ActiveMQConfig.java
@@ -9,11 +9,12 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import javax.jms.ConnectionFactory;
 
 /**
- * 如果需要在同⼀项目中既支持队列模式也支持广播模式，可以通过 DefaultJmsListenerContainerFactory 创
- * 建⾃定义的 JmsListenerContainerFactory 实例，之后在 @JmsListener 注解中通过 containerFactory 属性引
- * ⽤用它。
- * 分别创建两个⾃定义的 JmsListenerContainerFactory 实例，通过 pubSubDomain 来控制是支持队列模式还
- * 是广播模式
+ * 如果需要在同⼀项目中既支持队列模式也支持广播模式，
+ * 可以通过 DefaultJmsListenerContainerFactory 创
+ * 建⾃定义的 JmsListenerContainerFactory 实例，
+ * 之后在 @JmsListener 注解中通过 containerFactory 属性引⽤用它。
+ * 分别创建两个⾃定义的 JmsListenerContainerFactory 实例，
+ * 通过 pubSubDomain 来控制是支持队列模式还是广播模式
  * @author tom
  * @version V1.0
  * @date 2020/9/13 17:30
